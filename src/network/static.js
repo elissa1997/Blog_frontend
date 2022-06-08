02fa884ac9562ea4cfcc6a9190a9ff776c40cc05
elissa_liu <524948583@qq.com>
2022-06-08 09:30:32 +0800

    add: 添加静态资源网络请求

--- src/network/static.js
+++ src/network/static.js
@@ -2,7 +2,7 @@ import { instance_api } from '@/network/axios.js'
 
 export function dict(fileName) {
   return instance_api({
-    url: process.env.VUE_APP_STATIC_URL+'/dict/'+fileName+'.json',
+    url: process.env.VUE_APP_STATIC_URL+fileName+'.json',
     method: 'get',
   })
 }
