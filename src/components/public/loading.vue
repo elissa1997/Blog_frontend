99366714732b3dbbae8ee28da9f95a587deef937
elissa_liu <524948583@qq.com>
2022-06-11 22:30:58 +0800

    add: 添加公共组件

---
+++ src/components/public/loading.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="loading">
+    <div class="warp" :style="`height: ${height}`">
+      <a-spin size="large" :tip="text"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { spin } from 'ant-design-vue'
+
+export default {
+  name: "loading",
+  props: {
+    height: {
+      type: String,
+      default: "100%"
+    },
+    text: {
+      type: String,
+      default: "加载中..."
+    }
+  },
+  components: {
+    [spin.name]: spin
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {},
+  watch: {}
+}
+</script>
+<style lang="scss" scoped>
+#loading{}
+
+.warp {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
